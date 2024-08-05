72df7a7b4a8102efe2fab09b4e163aae64f22702
JulietaG13 <jgarberoglio@mail.austral.edu.ar>
2024-08-05 00:34:04 -0300

    Add body to ProgramNode and remove End

--- src/main/java/ast/ProgramNode.java
+++ src/main/java/ast/ProgramNode.java
@@ -1,24 +1,29 @@
 package ast;
 
 import ast.interfaces.Node;
+import ast.interfaces.StatementNode;
 import utils.LexicalRange;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProgramNode implements Node {
-  private final LexicalRange start;
-  private final LexicalRange end;
-  
-  public ProgramNode(LexicalRange start, LexicalRange end) {
-    this.start = start;
-    this.end = end;
+  private final List<StatementNode> body = new ArrayList<>();
+
+  public List<StatementNode> getBody() {
+    return body;
   }
-  
+
+  public void addStatement(StatementNode statement) {
+    body.add(statement);
+  }
+
   @Override
   public LexicalRange start() {
-    return start;
+    return new LexicalRange(0,0,0);
   }
   
   @Override
   public LexicalRange end() {
-    return end;
+    return null;  // TODO()
   }
 }
