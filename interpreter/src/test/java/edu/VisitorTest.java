ed9efa19065c2589e3e2d370b29504ebafffb317
inesgassiebayle <igassiebayle@mail.austral.edu.ar>
2024-08-09 17:41:20 -0300

    tests for the interpreter

--- interpreter/src/test/java/edu/VisitorTest.java
+++ interpreter/src/test/java/edu/VisitorTest.java
@@ -59,4 +59,68 @@ public class VisitorTest {
     }
     return outContent.toString();
   }
+
+  @Test
+  public void testPrintlnLiteralString() {
+    String code = "println(\"Hello World\");";
+
+    Lexer lexer = new Lexer(code);
+    lexer.tokenize();
+    List<Token> tokens = lexer.getTokens();
+
+    Parser parser = new Parser();
+    ProgramNode program = parser.parse(tokens);
+
+    VariableContext context = new VariableContext();
+    Reader reader = new Reader(new VariableContext());
+    ExecutionVisitor visitor = new ExecutionVisitor(reader);
+
+    String expectedOutput = "\"Hello World\"\n";
+    String actualOutput = getOutputFromInterpreter(() -> visitor.visit(program));
+    assertEquals(expectedOutput, actualOutput);
+  }
+
+  @Test
+  public void testPrintlnLiteralNumber() {
+    String code = "println(15);";
+
+    Lexer lexer = new Lexer(code);
+    lexer.tokenize();
+    List<Token> tokens = lexer.getTokens();
+
+    Parser parser = new Parser();
+    ProgramNode program = parser.parse(tokens);
+
+    VariableContext context = new VariableContext();
+    Reader reader = new Reader(new VariableContext());
+    ExecutionVisitor visitor = new ExecutionVisitor(reader);
+
+    double expectedOutput = 15.0;
+    String actualOutput = getOutputFromInterpreter(() -> visitor.visit(program));
+    assertEquals(expectedOutput, Double.parseDouble(actualOutput), 0.0);
+  }
+
+  @Test
+  public void testVariableDeclarationBinary() {
+    String code = "let age:Number = 10 + 5;";
+
+    Lexer lexer = new Lexer(code);
+    lexer.tokenize();
+    List<Token> tokens = lexer.getTokens();
+
+    Parser parser = new Parser();
+    ProgramNode program = parser.parse(tokens);
+
+    VariableContext context = new VariableContext();
+    Reader reader = new Reader(new VariableContext());
+    ExecutionVisitor visitor = new ExecutionVisitor(reader);
+
+    visitor.visit(program);
+
+    assertTrue(reader.hasNumberVariable("age"));
+    assertEquals(15, reader.getNumberVariable("age"));
+  }
+
+
+
 }
