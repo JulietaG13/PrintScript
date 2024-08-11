9ef3389225eed98403c97fc69fc994eac054b0b4
JulietaG13 <jgarberoglio@mail.austral.edu.ar>
2024-08-11 20:25:52 -0300

    Implement automatic testing for parsing functions

--- parser/src/test/java/edu/FunctionTest.java
+++ parser/src/test/java/edu/FunctionTest.java
@@ -1,77 +1,155 @@
 package edu;
 
 import edu.ast.ProgramNode;
+import edu.ast.expressions.CallExpressionNode;
+import edu.ast.expressions.IdentifierNode;
+import edu.ast.expressions.LiteralNumberNode;
+import edu.ast.expressions.LiteralStringNode;
+import edu.ast.interfaces.StatementNode;
+import edu.ast.statements.ExpressionStatementNode;
 import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static edu.TokenProvider.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class FunctionTest {
 
   @Test
   public void noArgs() {
+    String func = "println";
+
     List<Token> input = List.of(
-        getIdentifier("println"),
+        getIdentifier(func),
         getOpenParen(),
         getCloseParen(),
         getSemicolon()
     );
 
     ProgramNode root = new Parser().parse(input);
-    System.out.println(root);
+    List<StatementNode> body = root.getBody();
+
+    assertEquals(1, body.size());
+    assert body.getFirst() instanceof ExpressionStatementNode;
+
+    ExpressionStatementNode exp = (ExpressionStatementNode) body.getFirst();
+    assert exp.expression() instanceof CallExpressionNode;
+
+    CallExpressionNode call = (CallExpressionNode) exp.expression();
+    assertEquals(0, call.args().size());
+    assertEquals(func, call.callee().name());
   }
 
   @Test
   public void oneArg() {
+    String func = "println";
+    String arg = "hola";
+
     List<Token> input = List.of(
-        getIdentifier("println"),
+        getIdentifier(func),
         getOpenParen(),
-        getLiteral("hola"),
+        getLiteral(arg),
         getCloseParen(),
         getSemicolon()
     );
 
     ProgramNode root = new Parser().parse(input);
-    System.out.println(root);
+    List<StatementNode> body = root.getBody();
+
+    assertEquals(1, body.size());
+    assert body.getFirst() instanceof ExpressionStatementNode;
+
+    ExpressionStatementNode exp = (ExpressionStatementNode) body.getFirst();
+    assert exp.expression() instanceof CallExpressionNode;
+
+    CallExpressionNode call = (CallExpressionNode) exp.expression();
+    assertEquals(1, call.args().size());
+    assertEquals(func, call.callee().name());
+
+    assert call.args().getFirst() instanceof LiteralStringNode;
+    LiteralStringNode lit = (LiteralStringNode) call.args().getFirst();
+    assertEquals(arg, lit.value());
   }
 
   @Test
   public void twoArgs() {
+    String func = "println";
+    String arg1 = "hola";
+    Double arg2 = 123.0;
+
     List<Token> input = List.of(
-        getIdentifier("println"),
+        getIdentifier(func),
         getOpenParen(),
-        getLiteral("hola"),
+        getLiteral(arg1),
         getComma(),
-        getLiteral(123.0),
+        getLiteral(arg2),
         getCloseParen(),
         getSemicolon()
     );
 
     ProgramNode root = new Parser().parse(input);
-    System.out.println(root);
+    List<StatementNode> body = root.getBody();
+
+    assertEquals(1, body.size());
+    assert body.getFirst() instanceof ExpressionStatementNode;
+
+    ExpressionStatementNode exp = (ExpressionStatementNode) body.getFirst();
+    assert exp.expression() instanceof CallExpressionNode;
+
+    CallExpressionNode call = (CallExpressionNode) exp.expression();
+    assertEquals(2, call.args().size());
+    assertEquals(func, call.callee().name());
+
+    assert call.args().getFirst() instanceof LiteralStringNode;
+    LiteralStringNode lit1 = (LiteralStringNode) call.args().getFirst();
+    assertEquals(arg1, lit1.value());
+
+    assert call.args().get(1) instanceof LiteralNumberNode;
+    LiteralNumberNode lit2 = (LiteralNumberNode) call.args().get(1);
+    assertEquals(arg2, lit2.value());
   }
 
   @Test
   public void varAsArg() {
+    String func = "println";
+    String var = "var";
+
     List<Token> input = List.of(
-        getIdentifier("println"),
+        getIdentifier(func),
         getOpenParen(),
-        getIdentifier("var"),
+        getIdentifier(var),
         getCloseParen(),
         getSemicolon()
     );
 
     ProgramNode root = new Parser().parse(input);
-    System.out.println(root);
+    List<StatementNode> body = root.getBody();
+
+    assertEquals(1, body.size());
+    assert body.getFirst() instanceof ExpressionStatementNode;
+
+    ExpressionStatementNode exp = (ExpressionStatementNode) body.getFirst();
+    assert exp.expression() instanceof CallExpressionNode;
+
+    CallExpressionNode call = (CallExpressionNode) exp.expression();
+    assertEquals(1, call.args().size());
+    assertEquals(func, call.callee().name());
+
+    assert call.args().getFirst() instanceof IdentifierNode;
+    IdentifierNode id = (IdentifierNode) call.args().getFirst();
+    assertEquals(var, id.name());
   }
 
   @Test
   public void functionAsArg() {
+    String func = "println";
+    String nestedFunc = "foo";
+
     List<Token> input = List.of(
-        getIdentifier("println"),
+        getIdentifier(func),
         getOpenParen(),
-        getIdentifier("foo"),
+        getIdentifier(nestedFunc),
         getOpenParen(),
         getCloseParen(),
         getCloseParen(),
@@ -79,6 +157,21 @@ public class FunctionTest {
     );
 
     ProgramNode root = new Parser().parse(input);
-    System.out.println(root);
+    List<StatementNode> body = root.getBody();
+
+    assertEquals(1, body.size());
+    assert body.getFirst() instanceof ExpressionStatementNode;
+
+    ExpressionStatementNode exp = (ExpressionStatementNode) body.getFirst();
+    assert exp.expression() instanceof CallExpressionNode;
+
+    CallExpressionNode call = (CallExpressionNode) exp.expression();
+    assertEquals(1, call.args().size());
+    assertEquals(func, call.callee().name());
+
+    assert call.args().getFirst() instanceof CallExpressionNode;  // TODO(check)
+    CallExpressionNode call2 = (CallExpressionNode) call.args().getFirst();
+    assertEquals(0, call2.args().size());
+    assertEquals(nestedFunc, call2.callee().name());
   }
 }
