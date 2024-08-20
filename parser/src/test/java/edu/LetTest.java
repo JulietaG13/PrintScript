ea7981ab40239f3aa6be382b6a136717e4843c61
JulietaG13 <jgarberoglio@mail.austral.edu.ar>
2024-08-20 18:55:24 -0300

    Implement more tests for Parser

--- parser/src/test/java/edu/LetTest.java
+++ parser/src/test/java/edu/LetTest.java
@@ -1,57 +1,102 @@
 package edu;
 
-import static edu.TokenProvider.getColon;
-import static edu.TokenProvider.getEquals;
-import static edu.TokenProvider.getIdentifier;
-import static edu.TokenProvider.getLet;
-import static edu.TokenProvider.getLiteral;
-import static edu.TokenProvider.getNumber;
-import static edu.TokenProvider.getSemicolon;
+import static edu.TokenProvider.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import edu.ast.ProgramNode;
+import edu.ast.expressions.LiteralNumberNode;
+import edu.ast.expressions.LiteralStringNode;
+import edu.ast.interfaces.StatementNode;
+import edu.ast.statements.Type;
+import edu.ast.statements.VariableDeclarationNode;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class LetTest {
 
   @Test
-  public void noInitialValue() {
+  public void noInitialValue() { // let a : Number;
+    String var = "a";
     List<Token> input =
         List.of(getLet(), getIdentifier("a"), getColon(), getNumber(), getSemicolon());
 
     ProgramNode root = new Parser().parse(input);
-    System.out.println(root);
+    List<StatementNode> statements = root.getBody();
+    assertEquals(1, statements.size());
+
+    StatementNode statement = statements.getFirst();
+    assert statement instanceof VariableDeclarationNode;
+
+    VariableDeclarationNode declaration = (VariableDeclarationNode) statement;
+    assertEquals(var, declaration.id().name());
+
+    assertEquals(Type.NUMBER, declaration.type());
+    assertNull(declaration.init());
   }
 
   @Test
   public void initialNumberValue() {
+    String var = "var";
+    double value = 123.45;
+
     List<Token> input =
         List.of(
             getLet(),
-            getIdentifier("number-123"),
+            getIdentifier(var),
             getColon(),
             getNumber(),
             getEquals(),
-            getLiteral(123.0),
+            getLiteral(value),
             getSemicolon());
 
     ProgramNode root = new Parser().parse(input);
-    System.out.println(root);
+    List<StatementNode> statements = root.getBody();
+    assertEquals(1, statements.size());
+
+    StatementNode statement = statements.getFirst();
+    assert statement instanceof VariableDeclarationNode;
+
+    VariableDeclarationNode declaration = (VariableDeclarationNode) statement;
+    assertEquals(var, declaration.id().name());
+
+    assertEquals(Type.NUMBER, declaration.type());
+    assertNotNull(declaration.init());
+
+    assert declaration.init() instanceof LiteralNumberNode;
+    LiteralNumberNode literal = (LiteralNumberNode) declaration.init();
+    assertEquals(value, literal.value());
   }
 
   @Test
   public void initialStringValue() {
+    String var = "string-var";
+    String value = "aaabbbccc";
+
     List<Token> input =
         List.of(
             getLet(),
-            getIdentifier("string-hola"),
+            getIdentifier(var),
             getColon(),
-            getNumber(),
+            getString(),
             getEquals(),
-            getLiteral("hola"),
+            getLiteral(value),
             getSemicolon());
 
     ProgramNode root = new Parser().parse(input);
-    System.out.println(root);
+    List<StatementNode> statements = root.getBody();
+    assertEquals(1, statements.size());
+
+    StatementNode statement = statements.getFirst();
+    assert statement instanceof VariableDeclarationNode;
+
+    VariableDeclarationNode declaration = (VariableDeclarationNode) statement;
+    assertEquals(var, declaration.id().name());
+
+    assertEquals(Type.STRING, declaration.type());
+    assertNotNull(declaration.init());
+
+    assert declaration.init() instanceof LiteralStringNode;
+    LiteralStringNode literal = (LiteralStringNode) declaration.init();
+    assertEquals(value, literal.value());
   }
 }
