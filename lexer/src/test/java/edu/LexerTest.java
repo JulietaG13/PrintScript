c42dc997561cc8a9c93278e73532e91f91d9389a
inesgassiebayle <igassiebayle@mail.austral.edu.ar>
2024-08-20 14:58:23 -0300

    increase lexer coverage

--- lexer/src/test/java/edu/LexerTest.java
+++ lexer/src/test/java/edu/LexerTest.java
@@ -5,6 +5,8 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
+
+import edu.utils.LexicalRange;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -60,5 +62,18 @@ public class LexerTest {
     Lexer invalidLexer = new Lexer("let x = 42 @;");
     Exception exception = assertThrows(RuntimeException.class, invalidLexer::tokenize);
     assertTrue(exception.getMessage().contains("Invalid token"));
+  }
+
+  @Test
+  public void testLexicalRangeToString() {
+    LexicalRange range = new LexicalRange(15, 5, 1);
+    assertEquals("LexicalRange(offset=15, line=5, column=1)", range.toString());
+  }
+
+  @Test
+  public void testPatternManager() {
+    PatternManager patternManager = new PatternManager();
+    PatternManager patternManager1 = patternManager.addKeyword("let");
+
   }
 }
--- parser/src/main/java/edu/common/OperatorProvider.java
+++ parser/src/main/java/edu/common/OperatorProvider.java
@@ -13,10 +13,10 @@ public class OperatorProvider {
   private static final Map<Operator, String> strings = new HashMap<>();
 
   static {
-    add(SUM.symbol, SUM);
-    add(SUBTRACT.symbol, SUBTRACT);
-    add(MULTIPLY.symbol, MULTIPLY);
-    add(DIVIDE.symbol, DIVIDE);
+    add(Operator.SUM.symbol, Operator.SUM);
+    add(Operator.SUBTRACT.symbol, Operator.SUBTRACT);
+    add(Operator.MULTIPLY.symbol, Operator.MULTIPLY);
+    add(Operator.DIVIDE.symbol, Operator.DIVIDE);
   }
 
   public static Operator getOperator(String name) {
