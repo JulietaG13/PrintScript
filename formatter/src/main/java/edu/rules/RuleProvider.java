51ac809df24f6c7036b0bb5b96432056c3e3d054
JulietaG13 <jgarberoglio@mail.austral.edu.ar>
2024-08-20 00:20:17 -0300

    Implement RuleProvider to check the formatting rules

---
+++ formatter/src/main/java/edu/rules/RuleProvider.java
@@ -0,0 +1,37 @@
+package edu.rules;
+
+import java.util.Map;
+import java.util.Set;
+
+public class RuleProvider {
+  private final Set<String> spaceBefore;
+  private final Set<String> spaceAfter;
+  private final Map<String, Integer> newLineBefore;
+
+  public RuleProvider(
+      Set<String> spaceBefore, Set<String> spaceAfter, Map<String, Integer> newLineBefore) {
+    this.spaceBefore = spaceBefore;
+    this.spaceAfter = spaceAfter;
+    this.newLineBefore = newLineBefore;
+  }
+
+  public boolean hasSpaceBefore(String string) {
+    return spaceBefore.contains(string);
+  }
+
+  public boolean hasSpaceAfter(String string) {
+    return spaceAfter.contains(string);
+  }
+
+  public int newLineBefore(String string) {
+    return newLineBefore.get(string);
+  }
+}
+
+/* Configurable
+ * declaration_space_before_colon
+ * declaration_space_after_colon
+ * assignment_space_before_equals
+ * assignment_space_after_equals
+ * println_new_lines_before_call
+ */
